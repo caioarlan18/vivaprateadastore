4c8faac61ecb84e0d7f8ebed76670242e2c61c77
caioarlan18 <caioarlan58@gmail.com>
2024-05-04 01:14:46 -0300

    fixing backend again

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -3,7 +3,8 @@ const path = require("path");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "uploads/")
+        const uploadsPath = path.join(__dirname, "/opt/render/project/backend/src/uploads/");
+        cb(null, uploadsPath);
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname))
