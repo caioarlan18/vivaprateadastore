2495d06c4a4538642ffd46b767cfefa43101f3eb
caioarlan18 <caioarlan58@gmail.com>
2024-05-04 01:00:48 -0300

    teste de upload

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const uploadsPath = path.join(__dirname, "..", "..", "uploads");
+        const uploadsPath = path.join(__dirname, "uploads");
         cb(null, uploadsPath);
     },
     filename: function (req, file, cb) {
