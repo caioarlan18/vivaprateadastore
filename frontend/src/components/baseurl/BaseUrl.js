d3a657d44e18a7f879206b84f277c44c27993a5a
caioarlan18 <caioarlan58@gmail.com>
2024-11-12 22:28:17 -0300

    hospedando

--- frontend/src/components/baseurl/BaseUrl.js
+++ frontend/src/components/baseurl/BaseUrl.js
@@ -1,3 +1,3 @@
-const baseurl = "https://vivaprateadastore.vercel.app/";
+const baseurl = "https://vivaprateadastore-7od06hsw5-caioarlan18s-projects.vercel.app/?vercelToolbarCode=4KU7qbDrQceQEJD";
 
 export default baseurl;
